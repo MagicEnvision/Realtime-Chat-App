d05dae2f365e42bd223d86f7f0f9f03b309d3dcd
magicenvision <rjrose2003@gmail.com>
2024-12-27 15:10:24 -0500

    added bcrypted salt

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,5 +1,26 @@
-export const signup = (req, res) => {
-    res.send("Signup route")
+import User from "../models/user.models.js";
+import bcrypt from "bcryptjs"
+export const signup = async (req, res) => {
+
+    const {fullname, email, password} = req.body;
+    try {
+        if(password.length < 6){
+            return res.status(400).json({message: "password is less than 6 characters"})
+        }
+        const user = await User.findOne({email})
+        if (user) {
+            return res.status(400).json({message: "email already exists"})
+        }
+
+        const salt = await bcrypt.genSalt(10)
+
+        const hashedPassword = await bcrypt.hash(password, salt)
+        
+        
+        
+    } catch (error) {
+        
+    }
 }
 export const login = (req, res) => {
     res.send("Login route")
--- backend/src/index.js
+++ backend/src/index.js
@@ -12,6 +12,8 @@ const PORT = process.env.PORT;
 
 app.use("/api/auth", authRoutes)
 
+app.use(express.json())
+
 app.listen(PORT, () => {
     console.log("Server is running on port "+ PORT)
     connectDB();
