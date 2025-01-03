6bbef81ef6e4a6947fe08f15b3cd64f7bc6b408e
magicenvision <rjrose2003@gmail.com>
2025-01-03 14:23:03 -0500

    added protectRoute middleware

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv"
+import cookieParser from "cookie-parser"
 
 
 dotenv.config();
@@ -14,6 +15,7 @@ const PORT = process.env.PORT;
 
 app.use(express.json())
 app.use("/api/auth", authRoutes)
+app.use(cookieParser())
 app.listen(PORT, () => {
     console.log("Server is running on port "+ PORT)
     connectDB();
