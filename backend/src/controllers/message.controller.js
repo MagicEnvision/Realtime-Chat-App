95e99b3ae8ba26327aaac8749bb81b8f5ad7d3b1
magicenvision <rjrose2003@gmail.com>
2025-01-15 21:57:11 -0500

    added getMessages

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -1,4 +1,5 @@
 import User from "../models/user.models"
+import Message from "../models/message.model.js"
 
 export const getUsersForSidebar = async (req, res) => {
     try {
@@ -10,5 +11,26 @@ export const getUsersForSidebar = async (req, res) => {
         console.error("Error in getUsersForSideBar", error.message)
         res.status(500).json({error: "Internal Server Error"})
         
+    }
+}
+
+export const getMessages = async (req, res) => {
+    try{
+        const {id:userToChatId} = req.params;
+        const myId = req.user._id;
+
+        const messages = await Message.find({ //find messages, and these are the parameters
+
+            $or:[ // function to choose between the parameters
+                {senderId:myId, receiverId:userToChatId}, //where the senderid is mine and the receiverid is the user to chat with 
+                {senderId:userToChatId, reciever:myId} //where the senderid the the user i chatted with and the reciver is my user
+            ]
+        })
+
+        res.status(200).json(messages)
+
+    } catch (err){
+        console.log("Error in getMessages controller: ", error.message)
+        res.status(500).json({  error: "Internal Server Error"})
     }
 }
