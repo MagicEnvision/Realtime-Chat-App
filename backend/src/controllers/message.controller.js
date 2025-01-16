2923df566dfe0b9a090ac444a6490bd33c44cf75
magicenvision <rjrose2003@gmail.com>
2025-01-15 22:08:27 -0500

    added sendMessage

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -34,3 +34,34 @@ export const getMessages = async (req, res) => {
         res.status(500).json({  error: "Internal Server Error"})
     }
 }
+
+export const sendMessage = async (req, res) => {
+    try {
+        const {text, image} = req.body;
+        const {id: receiverId} = req.params;
+        const senderId = req.user_id;
+
+        let imageUrl;
+        if (image) {
+            const uploadedResponse = await cloudinary.uploader.upload(image);
+            imageUrl = uploadedResponse.secure_url;
+        }
+
+        const newMessage = new Message({
+            senderId, 
+            receiverId,
+            text,
+            image: imageUrl,
+        });
+        
+        await newMessage.save()
+
+        //todo: realtime functionality will be implemented here => socket.io
+
+        res.status(201).json(newMessage)
+    } catch (error) {
+        console.log("Error in sendMessage controller: ", error.message)
+        res.status(500).json({error: "Internal Server Error"})
+        
+    }
+}
