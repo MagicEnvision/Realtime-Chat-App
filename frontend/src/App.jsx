f1cc91682ff80cbbb86fae7d41276336320fbc83
magicenvision <rjrose2003@gmail.com>
2025-01-19 20:51:39 -0500

    created pages and added zustand and axios

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,11 @@
 import React from 'react'
-import NavBar from './components/navBar'
+import NavBar from './components/navBar.jsx'
+import HomePage from './pages/HomePage.jsx'
+import SignUpPage from './pages/SignUpPage.jsx'
+import LoginPage from './pages/LoginPage.jsx'
+import SettingsPage from './pages/SettingsPage.jsx'
+import ProfilePage from './pages/ProfilePage.jsx'
+
 import {Route, Routes} from 'react-router-dom'
 
 const App = () => {
